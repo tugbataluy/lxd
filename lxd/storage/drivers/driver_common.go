ad1fac9a7f284c87c8ab6d00e089af1b675945b1
tugbataluy <tugba.taluy@canonical.com>
2026-04-30 13:11:18 +0300

    lxd/storage/drivers/driver_common: Drop default CreateVolumeFromImage implementation.
    Signed-off-by: tugbataluy <tugba.taluy@canonical.com>

--- lxd/storage/drivers/driver_common.go
+++ lxd/storage/drivers/driver_common.go
@@ -376,11 +376,6 @@ func (d *common) CreateVolumeFromCopy(vol VolumeCopy, srcVol VolumeCopy, allowIn
 	return ErrNotSupported
 }
 
-// CreateVolumeFromImage creates volume from images.
-func (d *common) CreateVolumeFromImage(vol Volume, imgVol *Volume, filler *VolumeFiller, progressReporter ioprogress.ProgressReporter) error {
-	return ErrNotSupported
-}
-
 // CreateVolumeFromMigration creates a new volume (with or without snapshots) from a migration data stream.
 func (d *common) CreateVolumeFromMigration(vol VolumeCopy, conn io.ReadWriteCloser, volTargetArgs migration.VolumeTargetArgs, preFiller *VolumeFiller, progressReporter ioprogress.ProgressReporter) error {
 	return ErrNotSupported
